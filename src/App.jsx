530a8e63bcea825e8b44354d96cd82d0a65c378b
fernanver <fernanverstudy@gmail.com>
2026-05-29 10:30:44 -0500

    create the repository

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <section>
+      <div className="hero"></div>
+      <h1>Hola como estas</h1>
+    </section>
+  );
+}
+
+export default App;
